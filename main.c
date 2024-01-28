a84ec84286c1b1faa47b80abbf8432d68daf5c77
Kimia Khudsiani <kimia.khoodsiyani@gmail.com>
2024-01-28 20:38:20 +0330

    Yeah

---
+++ main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    char name[20];
+    char lastName[20];
+    char stuNum[20];
+    char cityBorn[20];
+}
+Stu;
+
+// Menu
+void read (); // 1
+void search (); // 2
+void addStu (int); // 3
+void deleteStu (int); // 4
+void randOrd (); // 5
+void sort (); // 6
+void write (); // 7
+void end (); // 8
+
+int main()
+{
+
+}
+
+void read ()
+{
+
+}
+
+void search ()
+{
+
+}
+
+void addStu (int row)
+{
+
+}
+
+void deleteStu (int row)
+{
+
+}
+
+void randOrd ()
+{
+
+}
+
+void sort ()
+{
+
+}
+
+void write ()
+{
+
+}
+
+void end ()
+{
+
+}
