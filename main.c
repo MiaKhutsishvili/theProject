c2c27869640d0504ebe15eeb19378f2925b30572
Kimia Khudsiani <kimia.khoodsiyani@gmail.com>
2024-01-28 21:11:51 +0330

    ok

--- main.c
+++ main.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-typedef struct
+typedef struct stu
 {
     char name[20];
     char lastName[20];
     char stuNum[20];
     char cityBorn[20];
+    struct stu * next;
+    struct stu * prev;
+
 }
 Stu;
+
+Stu * Last = NULL;
+Stu * First = NULL; 
+bool checkIfReadIsDone;
 
 // Menu
 void read (); // 1
 void search (); // 2
-void addStu (int); // 3
+Stu addStu (int); // 3
 void deleteStu (int); // 4
 void randOrd (); // 5
 void sort (); // 6
@@ -27,15 +35,40 @@ int main()
 
 void read ()
 {
-
+    checkIfReadIsDone = 1;
+    FILE * Fptr = fopen ("data.txt", "r");
+    if (Fptr == NULL)
+    {
+        printf ("Error in the file opening.\n");
+        return;
+    }
+    int n = 0;
+    while (!feof (Fptr))
+    {
+        Stu newStu = addStu (n);
+        n++;
+    }
+    printf ("Row:\t\tName:\t\tLast Name:\t\tStudent Number:\t\tMother City:\t\t\n");
+    Stu * current = First;
+    for (int i = 0; i < n; i++)
+    {   
+        printf ("%d\t\t", i);
+        printf ("%s\t\t", current->name);
+        printf ("%s\t\t", current->lastName);
+        printf ("%s\t\t", current->stuNum);
+        printf ("%s\t\t", current->cityBorn);
+        current = current->next;
+    }
+    fclose (Fptr);
+    return;
 }
 
 void search ()
 {
-
+    
 }
 
-void addStu (int row)
+Stu addStu (int row)
 {
 
 }
