c6fd689952a93b50e0b9d5a1ede89b3364095634
Kimia <152694792+MiaKhutsishvili@users.noreply.github.com>
2024-01-30 19:25:34 +0330

    Update two.c

--- two.c
+++ two.c
@@ -2,14 +2,38 @@
 #include <string.h>
 #include "definitions.h"
 
-Stu * fileSearch (char * ID)
+// 1 = name
+// 2 = last name
+// 3 = id
+// 4 = city
+
+void printStu (Stu *);
+
+void searchStu (int mode, char * str)
 {
     Stu * current = first;
     for (int i = 0; i < cnt; i++)
     {
-        if (!strcmp (current -> ID, ID))
-            break;
+
+        if (mode == 1 && !strcmp (current -> name, str))
+            printStu (current);
+        if (mode == 2 && !strcmp (current -> lastName, str))
+            printStu (current);
+        if (mode == 3 && !strcmp (current -> ID, str))
+            printStu (current);
+        if (mode == 4 && !strcmp (current -> city, str))
+            printStu (current);
         current = current -> next;
     }
-    return current;
+    return;
+}
+
+void printStu (Stu *output)
+{
+    printf ("Name: %s\t\t", output -> name);
+    printf ("Last name: %s\t\t", output -> lastName);
+    printf ("ID: %s\t\t", output -> ID);
+    printf ("City: %s\t\t", output -> city);
+    printf ("\n");
+    return;
 }
