62cbcda630cfaee8a61152fdcf8636e2345e8e58
Kimia <152694792+MiaKhutsishvili@users.noreply.github.com>
2024-01-30 19:24:45 +0330

    Update one.c

--- one.c
+++ one.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "definitions.h"
 
 void fileRead ()
 {
     FILE * fptr = fopen ("data.txt", "r");
     int i = 0;
     printf ("#\t\tName:\t\tLast name:\t\tID:\t\tCity:\t\t\n");
-    while (!feof (fptr))
+    while (i < fileCnt)
     {
         i++;
-        char str[100000];
-        fgets (str, 100000, fptr);
+        char str[1000];
+        fgets (str, 1000, fptr);
         printf ("%d\t\t%s", i, str);
     }
     fclose (fptr);
