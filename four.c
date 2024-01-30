e8045ee482a3d95d0d501e3653b2b3e25b4e86b5
Kimia <152694792+MiaKhutsishvili@users.noreply.github.com>
2024-01-30 19:24:08 +0330

    Update four.c

--- four.c
+++ four.c
@@ -3,21 +3,11 @@
 #include <string.h>
 #include "definitions.h"
 
-void stuRemove (char * ID)
+void removeStu (Stu * stu)
 {
-    int line = 0;
-    // Removing from the Linked List
-    Stu * current = first;
-    for (int i = 0; i < cnt; i++)
-    {
-        line++;
-        if (!strcmp (current -> ID, ID))
-            break;
-        current = current -> next;
-    }
-    current -> prev -> next = current -> next;
-    current -> next -> prev = current -> prev;
-    free (current);
+    stu -> prev -> next = stu -> next;
+    stu -> next -> prev = stu -> prev;
+    free (stu);
 
     cnt--;
     return;
