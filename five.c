586110144ed02548b6cd03861bb7725c34598722
Kimia <152694792+MiaKhutsishvili@users.noreply.github.com>
2024-01-31 16:09:00 +0330

    Update five.c

--- five.c
+++ five.c
@@ -5,65 +5,42 @@
 #include <time.h>
 #include "definitions.h"
 
-void dfs (int);
-Stu cash;
-Stu cash1;
 bool check[1000];
-int order[1000];
-Stu * address[1000];
+Stu * before[1000];
+Stu * after[1000];
 
 void shuffle ()
 {
     for (int i = 0; i < 1000; i++)
         check[i] = 0;
     srand (time (NULL));
-    for (int i = 0; i < cnt; i++)
-    {
-        do
-            order[i] = rand() % cnt;
-        while (check[order[i]] == 1);
-    }
-
-    // Changing the Linked List
+    
     Stu * current = first;
-    // Saving the addresses
-    for (int i = 0; i < cnt; i++)
+    for (int i = 1; i <= cnt; i++)
     {
-        address[i] = current;
+        before[i] = current;
         current = current -> next;
     }
-    for (int i = 0; i < 1000; i++)
-        check[i] = 0;
-    for (int i = 0; i < cnt; i++)
+    for (int i = 1; i <= cnt; i++)
     {
-        if (!check[i])
-            dfs (i);
-        check[i] = 1;
+        int shuffle;
+        do
+            shuffle = rand() % cnt + 1;
+        while (check[shuffle]);
+        check[shuffle] = 1;
+        after[i] = before[shuffle];
     }
-
+    after[0] = NULL;
+    after[cnt + 1] = NULL;
+    first = after[1];
+    head = after[cnt];
+    current = first;
+    for (int i = 1; i <= cnt; i++)
+    {
+        current -> prev = after[i - 1];
+        current -> next = after[i + 1];
+        current = current -> next;
+    }
     return;
 }
 
-void dfs (int node)
-{
-    strcpy (cash1.name, address[node] -> name);
-    strcpy (cash1.lastName, address[node] -> lastName);
-    strcpy (cash1.ID, address[node] -> ID);
-    strcpy (cash1.city, address[node] -> city);
-
-    strcpy (address[node] -> name , cash.name);
-    strcpy (address[node] -> lastName, cash.lastName);
-    strcpy (address[node] -> ID, cash.ID);
-    strcpy (address[node] -> city, cash.city);
-
-    strcpy (cash.name, cash1.name);
-    strcpy (cash.lastName, cash1.lastName);
-    strcpy (cash.ID, cash1.ID);
-    strcpy (cash.city, cash1.city);
-
-    if (check[node])
-        return;
-
-    dfs (order[node]);
-    return;
-}
