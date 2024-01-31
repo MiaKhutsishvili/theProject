14eaf0ebd10b3a86fe06cf0c434416e7127ee21c
Kimia <152694792+MiaKhutsishvili@users.noreply.github.com>
2024-01-31 18:29:38 +0330

    Update Main.c

--- Main.c
+++ Main.c
@@ -18,10 +18,10 @@ void searchStu (int, char *); //2 +
 void addStu (int, Stu *); //3 + 
 void removeStu (Stu *); //4 +
 void shuffle (); //5 +
-void selectionSort (bool); //6 +     // Don't Forget to measure time taken!
-void insertionSort (bool); //6 +
-void bubbleSort (bool); //6 +
-void quickSort (bool, Stu *, Stu *); //6 +
+void selectionSort (int); //6 +     // Don't Forget to measure time taken!
+void insertionSort (int); //6 +
+void bubbleSort (int); //6 +
+void quickSort (int, int, int); //6 +
 void fileWrite (); //7 +
 void progEnd (); //8 +
 
@@ -29,6 +29,8 @@ void progEnd (); //8 +
 
 void displayStu (Stu *);
 void displayAll ();
+void addressing ();
+void update ();
 
 int main()
 {
@@ -40,7 +42,7 @@ int main()
     printf ("Enter:\n");
     printf ("1 -> read the file\n");
     printf ("2 -> search the file\n");
-    printf ("3 -> add a studen\n");
+    printf ("3 -> add a student\n");
     printf ("4 -> remove a student\n");
     printf ("5 -> shuffle the list\n");
     printf ("6 -> sort the list\n");
@@ -145,11 +147,12 @@ int main()
         printf ("Bubble sort -> 3\n");
         printf ("Quick sort -> 4\n");
         scanf ("%d", &mode);
-        bool way;
+        int way;
         printf ("Enter:\n");
-        printf ("In a growing way -> 1\n");
-        printf ("Else -> 0\n");
-        scanf ("%d", way); 
+        printf ("Up growing -> 1\n");
+        printf ("Down growing -> 0\n");
+        scanf ("%d", &way); 
+        addressing();
         clock_t start, end;
         double takenTime;
         start = clock();
@@ -160,9 +163,10 @@ int main()
         if (mode == 3)
             bubbleSort (way);
         if (mode == 4)
-            quickSort (way, first, head);
+            quickSort (way, 1, cnt);
         end = clock();  
         takenTime = ((double) (end - start)) / CLOCKS_PER_SEC;
+        update();
         displayAll();
         printf ("I took %lf seconds to sort.\n", takenTime);
     }
@@ -210,6 +214,32 @@ void displayAll ()
     for (int i = 0; i < cnt; i++)
     {
         display (current);
+        current = current -> next;
+    }
+    return;
+}
+
+void addressing ()
+{
+    address[0] = NULL;
+    address[cnt + 1] = NULL;
+    Stu * current = first;
+    for (int i = 1; i <= cnt; i++)
+    {
+        address[i] = current;
+        current = current -> next;
+    }
+    return;
+}
+void update ()
+{
+    first = address[1];
+    head = address[cnt];
+    Stu * current = first;
+    for (int i = 1; i <= cnt; i++)
+    {
+        current -> prev = address[i - 1];
+        current -> next = address[i + 1];
         current = current -> next;
     }
     return;
